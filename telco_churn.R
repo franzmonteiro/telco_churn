9b0a2b3dac9e6205605bb4bdb8b5a7537e225f4f
lmonteiro <lmonteiro@uolinc.com>
2022-03-06 12:46:48 -0300

    ajustes

--- telco_churn.R
+++ telco_churn.R
@@ -8,7 +8,7 @@ library(glmmTMB)
 library(lmtest)
 library(Information)
 
-tc <- list.files('csvs/churn/', pattern = '*.xlsx', full.names = T) %>% 
+tc <- list.files('csvs/', pattern = '*.xlsx', full.names = T) %>% 
     map(read_xlsx) %>% 
     map(rename_with, ~ str_to_lower(str_replace_all(.x, ' ', '_'))) %>% 
     map(select, !any_of(c('count'))) %>% 
@@ -38,9 +38,9 @@ xpto <- tmp$Tables %>%
     map(~ mutate(.x, nome_var = names(.x)[1])) %>% 
     map(~ rename(.x, grupo = 1)) %>% 
     reduce(rbind) %>% 
+    mutate(grupo = as_factor(grupo)) %>% 
     group_by(nome_var) %>% 
-    mutate(grupo = as_factor(grupo),
-           max_iv = max(IV),
+    mutate(max_iv = max(IV),
            n_item = row_number()) %>% 
     ungroup() %>% 
     arrange(desc(max_iv)) %>% 
@@ -73,30 +73,41 @@ train_glmm <- function(efeitos_fixos, efeitos_aleatorios) {
         {
             formula_str <- glue('flg_churn ~ {efeitos_fixos} + {efeitos_aleatorios}')
             
-            list(modelo = glmmTMB(as.formula(formula_str),
-                                  data = tc_train,
-                                  family = binomial, 
-                                  REML = T),
-                 efeitos_fixos = efeitos_fixos,
-                 efeitos_aleatorios = efeitos_aleatorios,
-                 error = NA,
-                 warning = NA)
+            modelo <- glmmTMB(as.formula(formula_str),
+                             data = tc_train,
+                             family = binomial, 
+                             REML = T)
+            
+            list(modelo = modelo,
+                 statistics = tibble(efeitos_fixos = efeitos_fixos,
+                                     efeitos_aleatorios = efeitos_aleatorios,
+                                     log_lik = as.numeric(logLik(modelo)),
+                                     aic = AIC(modelo),
+                                     error = NA,
+                                     warning = NA)
+            )
         },
         error=function(cond) {
             message("message:", cond)
             list(modelo = NA,
-                 efeitos_fixos = efeitos_fixos,
-                 efeitos_aleatorios = efeitos_aleatorios,
-                 error = as.character(cond),
-                 warning = NA)
+                 statistics = tibble(efeitos_fixos = efeitos_fixos,
+                                     efeitos_aleatorios = efeitos_aleatorios,
+                                     log_lik = NA,
+                                     aic = NA,
+                                     error = as.character(cond),
+                                     warning = NA)
+            )
         },
         warning=function(cond) {
             message("message:", cond)
             list(modelo = NA,
-                 efeitos_fixos = efeitos_fixos,
-                 efeitos_aleatorios = efeitos_aleatorios,
-                 error = NA,
-                 warning = as.character(cond))
+                 statistics = tibble(efeitos_fixos = efeitos_fixos,
+                                     efeitos_aleatorios = efeitos_aleatorios,
+                                     log_lik = NA,
+                                     aic = NA,
+                                     error = NA,
+                                     warning = as.character(cond))
+            )
         },
         finally={
             message(formula_str)
@@ -109,10 +120,11 @@ train_glmm <- function(efeitos_fixos, efeitos_aleatorios) {
 dep_vars <- tc_train %>%
     select(matches('_gmc') | !where(is.numeric)) %>%
     select(!matches(paste(discarded_dep_vars$Variable, collapse = '|'))) %>% 
-    select(-flg_churn) %>% 
+    select(-c(flg_churn, zip_code)) %>% 
     colnames() %>% 
-    map_dfr(train_glmm)
-
+    map(~ train_glmm(.x, '(1 | zip_code)')) %>% 
+    map_dfr(~ .x$statistics) %>% 
+    arrange(desc(log_lik))
 
 filter(dep_vars, !is.na(coalesce(warning, error)))
 
@@ -122,30 +134,29 @@ m0 <- glmmTMB(flg_churn ~ (1 | zip_code),
               family = binomial,
               REML = T)
 
-paste(dep_vars$var_preditora, collapse = ' + ')
+paste(dep_vars$efeitos_fixos, collapse = ' + ')
 
-# Funcao pra dividir o passo a passo da formacao de um perfil de estudo,
-# por padrao, os passos sao separados por ;;
 dep_vars_to_test <- dep_vars %>% 
     filter(is.na(coalesce(warning, error))) %>% 
-    filter(!var_preditora %in% c('zip_code', 'internet_type'))
+    filter(!efeitos_fixos %in% c('zip_code', 'internet_type'))
 
-dep_vars_to_test <- dep_vars_to_test$var_preditora
+dep_vars_to_test <- dep_vars_to_test$efeitos_fixos
 
-
-ci_models <- map(12:length(dep_vars_to_test), ~ paste(dep_vars_to_test[1:.x], collapse = ' + ')) %>%
-    # map(1:length(dep_vars_to_test), ~ paste(dep_vars_to_test[1:.x], collapse = ' + ')) %>% 
-    map_dfr(~ train_glmm(.x, '(1 | zip_code)'))
-
+# Doses homeopaticas.
+# A cada interacao insere uma nova variavel dependente no modelo,
+# De modo a descobrir variaveis que geram problema na convergencia.
+ci_models <- map(1:length(dep_vars_to_test), ~ paste(dep_vars_to_test[1:.x], collapse = ' + ')) %>%
+    map(~ train_glmm(.x, '(1 | zip_code)')) %>% 
+    map_dfr(~ .x$statistics)
 
 paste(dep_vars_to_test, collapse = ' + ')
 
 cim <- glmmTMB(flg_churn ~
                    age_gmc + number_of_dependents_gmc + zip_code_population_gmc + number_of_referrals_gmc + tenure_in_months_gmc 
-               + avg_monthly_gb_download_gmc + monthly_charge_gmc + total_charges_gmc + total_long_distance_charges_gmc + total_revenue_gmc 
-               + cltv_gmc + married + offer + internet_service + online_security + online_backup + device_protection_plan + premium_tech_support
-               + streaming_tv + unlimited_data + contract + paperless_billing + payment_method
-               + (1 | zip_code),
+                    + avg_monthly_gb_download_gmc + monthly_charge_gmc + total_charges_gmc + total_long_distance_charges_gmc + total_revenue_gmc 
+                    + cltv_gmc + married + offer + internet_service + online_security + online_backup + device_protection_plan + premium_tech_support
+                    + streaming_tv + unlimited_data + contract + paperless_billing + payment_method
+                    + (1 | zip_code),
                data = tc_train,
                family = binomial,
                REML = T)
@@ -155,9 +166,9 @@ lrtest(m0, cim)
 ai_models <- dep_vars_to_test %>% 
     map(~ train_glmm(paste(dep_vars_to_test, collapse = ' + '), glue('(1 + {.x} || zip_code)')))
 
-log_liks <- map_dfr(ai_models, ~ data.frame(efeitos_aleatorios = .x$efeitos_aleatorios,
-                                            log_lik = unique(ifelse(is.na(.x$modelo), NA, as.numeric(logLik(.x$modelo)))),
-                                            aic = unique(ifelse(is.na(.x$modelo), NA, AIC(.x$modelo)))))
+log_liks <- map_dfr(ai_models, ~ tibble(efeitos_aleatorios = .x$efeitos_aleatorios,
+                                        log_lik = unique(ifelse(is.na(.x$modelo), NA, as.numeric(logLik(.x$modelo)))),
+                                        aic = unique(ifelse(is.na(.x$modelo), NA, AIC(.x$modelo)))))
 
 log_liks %>% filter(log_lik == max(log_lik, na.rm = T))
 
